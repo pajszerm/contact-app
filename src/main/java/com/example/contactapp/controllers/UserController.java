d5f311c2e24a9708380594d17cd4544df32620a3
DAndris <pajszerm5@gmail.com>
2024-03-19 15:07:16 +0100

    refactoring

--- src/main/java/com/example/contactapp/controllers/UserController.java
+++ src/main/java/com/example/contactapp/controllers/UserController.java
@@ -6,14 +6,10 @@ import com.example.contactapp.validators.UserInfoValidator;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/api/users")
 public class UserController {
 
