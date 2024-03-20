28dd355098ea55ca25468cd6d0e47fc8fbcd08dc
DAndris <pajszerm5@gmail.com>
2024-03-20 14:11:41 +0100

    getUserDetails created

--- angular-frontend/src/app/services/user.service.ts
+++ angular-frontend/src/app/services/user.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
 import {UserFormModel} from "../models/userForm.model";
 import {Observable} from "rxjs";
+import {PageEvent} from "@angular/material/paginator";
+import {UserDetailsResponseModel} from "../models/userDetailsResponse.model";
 
 const BASE_URL = 'http://localhost:8080/api/users'
 
@@ -13,6 +15,13 @@ export class UserService {
   constructor(private http: HttpClient) { }
 
   saveUserForm(data: UserFormModel): Observable<any> {
-    return this.http.post(BASE_URL + '/new-contact', data)
+    return this.http.post('${BASE_URL/new-contact}', data)
+  }
+
+  getUserDetails(event: PageEvent): Observable<UserDetailsResponseModel> {
+    const params = new HttpParams()
+      .set('page', event.pageIndex.toString())
+      .set('size', event.pageSize.toString())
+    return this.http.get<UserDetailsResponseModel>(`${BASE_URL}/all}`, {params})
   }
 }
