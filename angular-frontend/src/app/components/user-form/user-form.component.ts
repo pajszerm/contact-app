c1b139f489c08334a8e976c5b0b15e177858e239
DAndris <pajszerm5@gmail.com>
2024-03-20 12:59:06 +0100

    validations on frontend

--- angular-frontend/src/app/components/user-form/user-form.component.ts
+++ angular-frontend/src/app/components/user-form/user-form.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormArray, FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AbstractControl, FormArray, FormBuilder, FormGroup, ValidatorFn, Validators} from "@angular/forms";
 import {Router} from "@angular/router";
 import {UserService} from "../../services/user.service";
 import {UserFormModel} from "../../models/userForm.model";
@@ -19,13 +19,13 @@ export class UserFormComponent implements OnInit {
               private router: Router,
               private userService: UserService) {
     this.userForm = formBuilder.group({
-      username: ['', Validators.required],
-      name: ['', Validators.required],
-      mothersName: ['', Validators.required],
-      email: [''],
-      birthDate: [null, Validators.required],
-      tajNumber: ['', Validators.required],
-      taxNumber: ['', Validators.required],
+      username: ['', [Validators.required, this.lettersAndSpacesValidator()]],
+      name: ['', [Validators.required, this.lettersAndSpacesValidator()]],
+      mothersName: ['', [Validators.required, this.lettersAndSpacesValidator()]],
+      email: ['', Validators.email],
+      birthDate: [null, Validators.required,],
+      tajNumber: ['', [Validators.required, this.tajNumberValidator()]],
+      taxNumber: ['', [Validators.required, this.taxNumberValidator()]],
       phoneNumbers: this.formBuilder.array([]),
       addresses: this.formBuilder.array([])
     })
@@ -48,16 +48,16 @@ export class UserFormComponent implements OnInit {
 
   addPhoneNumber() {
     (this.userForm.get('phoneNumbers') as FormArray).push(this.formBuilder.group({
-      phoneNumber: ['', Validators.required]
+      phoneNumber: ['', [Validators.required, this.onlyNumbersValidator()]]
     }));
   }
 
   addAddress() {
     (this.userForm.get('addresses') as FormArray).push(this.formBuilder.group({
-      cityName: ['', Validators.required],
-      streetName: ['', Validators.required],
-      houseNumber: ['', Validators.required],
-      postalCode: ['', Validators.required]
+      cityName: ['', [Validators.required, this.lettersAndSpacesValidator()]],
+      streetName: ['', [Validators.required, this.lettersAndSpacesValidator()]],
+      houseNumber: ['', Validators.required, [this.onlyNumbersValidator()]],
+      postalCode: ['', [Validators.required, this.postalCodeValidator()]]
     }));
   }
 
@@ -81,4 +81,48 @@ export class UserFormComponent implements OnInit {
     const addresses = this.userForm.get('addresses') as FormArray;
     return addresses.length > 0;
   }
+
+   tajNumberValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const value = control.value;
+      const isNumeric = /^\d+$/.test(value);
+      const isValidLength = value && value.length === 9;
+      return isNumeric && isValidLength ? null : { 'invalidTajNumber': true };
+    };
+  }
+
+  taxNumberValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const value = control.value;
+      const isNumeric = /^\d+$/.test(value);
+      const isValidLength = value && value.length === 8;
+      return isNumeric && isValidLength ? null : {'invalidTaxNumber': true};
+    };
+  }
+
+  postalCodeValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const postalCode = control.value;
+      const isValid = /^\d{4}$/.test(postalCode);
+      return isValid ? null : {'invalidPostalCode': {value: postalCode}};
+    };
+  }
+
+  onlyNumbersValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const houseNumber = control.value;
+      const isValid = /^\d+$/.test(houseNumber);
+      return isValid ? null : {'invalidHouseNumber': {value: houseNumber}};
+    };
+  }
+
+  lettersAndSpacesValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const value = control.value;
+      const isValid = /^[a-zA-Z\s]*$/.test(value); // Check if value contains only letters and spaces
+
+      return isValid ? null : {'invalidLettersAndSpaces': {value}};
+    };
+  }
+
 }
