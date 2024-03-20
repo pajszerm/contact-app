d25984ddf0de8fc1636a40a632614d70174262f7
DAndris <pajszerm5@gmail.com>
2024-03-20 14:12:07 +0100

    user-list component in progress

--- angular-frontend/src/app/components/user-list/user-list.component.ts
+++ angular-frontend/src/app/components/user-list/user-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {PageEvent} from "@angular/material/paginator";
+import {UserDetailsModel} from "../../models/userDetails.model";
+import {UserService} from "../../services/user.service";
 
 @Component({
   selector: 'app-user-list',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserListComponent implements OnInit {
 
-  constructor() { }
+  users!: UserDetailsModel[];
+  pageSizeOptions: number[] = [3, 6, 9];
+  pageIndex: number = 0;
+  pageSize: number = 0;
+  length: number = 0;
+  defaultPageEvent: PageEvent = {pageSize: 6, pageIndex: 0, length: 0};
+
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.getServerData(this.defaultPageEvent);
   }
 
+  handlePageEvent($event: PageEvent) {
+
+  }
+
+  private getServerData(defaultPageEvent: PageEvent) {
+
+  }
 }
