e3cd0b8e3325039e12afcab41b2b465e42c06e40
DAndris <pajszerm5@gmail.com>
2024-03-20 17:12:05 +0100

    delete function

--- angular-frontend/src/app/services/user.service.ts
+++ angular-frontend/src/app/services/user.service.ts
@@ -15,13 +15,17 @@ export class UserService {
   constructor(private http: HttpClient) { }
 
   saveUserForm(data: UserFormModel): Observable<any> {
-    return this.http.post('${BASE_URL/new-contact}', data)
+    return this.http.post(`${BASE_URL}/new-contact`, data);
   }
 
   getUserDetails(event: PageEvent): Observable<UserDetailsResponseModel> {
     const params = new HttpParams()
       .set('page', event.pageIndex.toString())
-      .set('size', event.pageSize.toString())
-    return this.http.get<UserDetailsResponseModel>(`${BASE_URL}/all}`, {params})
+      .set('size', event.pageSize.toString());
+    return this.http.get<UserDetailsResponseModel>(`${BASE_URL}/all`, { params });
+  }
+
+  deleteUserDetails(username: string | undefined): Observable<void> {
+    return this.http.delete<void>(`${BASE_URL}/delete/${username}`);
   }
 }
